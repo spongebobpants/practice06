2b214db3ab95163d2e178b65a8930c192b1ce2b7
spongebobpants <delay516@gmail.com>
2021-12-05 15:11:40 +0900

    music

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -2,6 +2,23 @@ package com.javaex.ex06;
 
 public class MyBase extends Base{
 
-    //코드작성
+	public void service(String state){
+		
+		if(state.equals("오후")) {
+			afternoon();
+		}else {
+			super.service(state);
+		}
+	}
+	
+	public void day() {
+		System.out.println("낮에는 열심히 수업듣자");
+	}
+
+	public void afternoon() {
+		System.out.println("오후에도 낮과 똑같이 공부해야합니다");
+	}
+	
+	
     
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,12 +3,24 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
-        
+        if("앱".equals(str)) {
+        	application();
+        }else if("음악".equals(str)){
+        	super.execute(str);
+        }else{
+        	super.execute(str);
+        	
+        }
         //코드작성
         
     }
- 
+    public void application() {
+		System.out.println("앱실행");
+	}
     //메소드작성
+    public void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
     
     //메소드작성
     
